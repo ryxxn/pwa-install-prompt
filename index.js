ba1e3453f1cbfd4b2bd44b12799cd1ea99da0d0e
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-07-28 04:05:09 +0900

    fix: remove unused code

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function getFaviconHref() {
-    const linkElements = document.getElementsByTagName('link');
-  
-    for (let i = 0; i < linkElements.length; i++) {
-      if (linkElements[i].getAttribute('rel') === 'icon') {
-        return linkElements[i].getAttribute('href');
-      }
+  const linkElements = document.getElementsByTagName('link');
+
+  for (let i = 0; i < linkElements.length; i++) {
+    if (linkElements[i].getAttribute('rel') === 'icon') {
+      return linkElements[i].getAttribute('href');
     }
-  
-    return '';
   }
-  
-  const MODAL_STYLE = `
+
+  return '';
+}
+
+const MODAL_STYLE = `
       .modal-overlay {
         display: none;
         position: fixed;
@@ -72,43 +72,39 @@ function getFaviconHref() {
         text-decoration: underline;
       }
     `;
-  
-  function createProxy() {
-    // 상태 객체
-    let state = {
-      isInstalled: true,
-    };
-  
-    // 상태 변화를 감지하기 위한 핸들러
-    const handler = {
-      set: function (target, property, value) {
-        console.log(
-          `${property}가 ${target[property]}에서 ${value}(으)로 변경되었습니다.`
-        );
-        target[property] = value;
-        // 상태 변경 후 추가 동작
-        if (property === 'isInstalled') {
-          if (value) {
-            console.log('PWA is already installed');
-          } else {
-            console.log('PWA is not installed');
-          }
+
+function createProxy() {
+  // 상태 객체
+  let state = {
+    isInstalled: true,
+  };
+
+  // 상태 변화를 감지하기 위한 핸들러
+  const handler = {
+    set: function (target, property, value) {
+      target[property] = value;
+      if (property === 'isInstalled') {
+        if (value) {
+          // 'PWA is already installed'
+        } else {
+          // 'PWA is not installed'
         }
-        return true;
-      },
-    };
-  
-    // Proxy
-    const proxyState = new Proxy(state, handler);
-  
-    return proxyState;
-  }
-  
-  const renderContent = () => {
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-  
-    if (isIOS) {
-      return `
+      }
+      return true;
+    },
+  };
+
+  // Proxy
+  const proxyState = new Proxy(state, handler);
+
+  return proxyState;
+}
+
+const renderContent = () => {
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  if (isIOS) {
+    return `
         <div class="modal-content">
           <div class="modal-body">
             <h1>IOS 앱 설치 방법</h1>
@@ -140,8 +136,8 @@ function getFaviconHref() {
           <button id="ok-button">괜찮아요, 모바일 웹으로 볼게요.</button>
         </div>
       `;
-    }
-    return `
+  }
+  return `
       <div class="modal-content">
         <div class="modal-body">
           <h1>앱으로 설치하기</h1>
@@ -154,91 +150,90 @@ function getFaviconHref() {
         </button>
       </div>
     `;
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Create and append styles
+  const style = document.createElement('style');
+  style.textContent = MODAL_STYLE;
+
+  document.head.appendChild(style);
+
+  // Create container
+  var container = document.createElement('div');
+  container.id = 'wepp-install-modal';
+  container.className = 'modal-overlay';
+  container.innerHTML = renderContent();
+
+  document.body.appendChild(container);
+
+  // Get the modal
+  const modal = document.getElementById('wepp-install-modal');
+
+  const onClose = () => {
+    window.location.hash = '';
   };
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    // Create and append styles
-    const style = document.createElement('style');
-    style.textContent = MODAL_STYLE;
-  
-    document.head.appendChild(style);
-  
-    // Create container
-    var container = document.createElement('div');
-    container.id = 'wepp-install-modal';
-    container.className = 'modal-overlay';
-    container.innerHTML = renderContent();
-  
-    document.body.appendChild(container);
-  
-    // Get the modal
-    const modal = document.getElementById('wepp-install-modal');
-  
-    const onClose = () => {
-      window.location.hash = '';
-    };
-  
-    modal.addEventListener('click', onClose);
-    document.getElementById('ok-button')?.addEventListener('click', onClose);
-    modal.querySelector('.modal-content').addEventListener('click', (e) => {
-      e.stopPropagation();
-    });
-  
-    const handleHashChange = () => {
-      const hash = window.location.hash;
-      const isValidHash = hash.startsWith('#wepp-install-modal');
-  
-      const nameElement = document.getElementById('wepp-name');
-      const logoElement = document.getElementById('wepp-logo');
-  
-      const name = document.title;
-      const logo = getFaviconHref();
-  
-      nameElement.textContent = name;
-      logoElement.src = logo;
-  
-      modal.style.display = isValidHash ? 'flex' : 'none';
-    };
-  
-    window.addEventListener('hashchange', handleHashChange);
-    handleHashChange(); // Run on initial load
-  
-    // ----------------------------------------------------------
-    // pwa install
-    const state = createProxy();
-    let deferredPrompt;
-  
-    window.addEventListener('beforeinstallprompt', (e) => {
-      e.preventDefault();
-      deferredPrompt = e;
-      state.isInstalled = false;
-    });
-  
-    const installButton = document.getElementById('wepp-install-button');
-    installButton.style.display = 'block';
-  
-    installButton.addEventListener('click', () => {
-      try {
-        // Show the install prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            // User accepted the install prompt
-            onClose();
-          } else {
-            // User dismissed the install prompt
-          }
-          // Clear the deferredPrompt variable
-          deferredPrompt = null;
-        });
-      } catch (error) {
-        console.log('Error: ', error);
-      }
-    });
-  
-    window.addEventListener('appinstalled', () => {
-      console.log('PWA was installed');
-    });
+
+  modal.addEventListener('click', onClose);
+  document.getElementById('ok-button')?.addEventListener('click', onClose);
+  modal.querySelector('.modal-content').addEventListener('click', (e) => {
+    e.stopPropagation();
   });
-  
+
+  const handleHashChange = () => {
+    const hash = window.location.hash;
+    const isValidHash = hash.startsWith('#wepp-install-modal');
+
+    const nameElement = document.getElementById('wepp-name');
+    const logoElement = document.getElementById('wepp-logo');
+
+    const name = document.title;
+    const logo = getFaviconHref();
+
+    nameElement.textContent = name;
+    logoElement.src = logo;
+
+    modal.style.display = isValidHash ? 'flex' : 'none';
+  };
+
+  window.addEventListener('hashchange', handleHashChange);
+  handleHashChange(); // Run on initial load
+
+  // ----------------------------------------------------------
+  // pwa install
+  const state = createProxy();
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    state.isInstalled = false;
+  });
+
+  const installButton = document.getElementById('wepp-install-button');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    try {
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          // User accepted the install prompt
+          onClose();
+        } else {
+          // User dismissed the install prompt
+        }
+        // Clear the deferredPrompt variable
+        deferredPrompt = null;
+      });
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  });
+
+  window.addEventListener('appinstalled', () => {
+    console.log('PWA was installed');
+  });
+});
