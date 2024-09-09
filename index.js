1e2ceba880bed19b1fdcdf133cababe16fa1a30f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-09-09 14:11:02 +0900

    chore: Update modal styles for better display on iOS devices

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // constants
 const MODAL_STYLE = `
   .modal-overlay { display: none; position: fixed; top: 0; left: 0; right: 0; bottom: 0; background: rgba(0, 0, 0, .5); justify-content: center; align-items: center; z-index: 99999; }
-  .modal-content { min-width: 300px; background: #fff; border-radius: 10px; position: relative; }
+  .modal-content { min-width: 300px; max-width:340px; background: #fff; border-radius: 10px; position: relative; box-sizing:border-box; }
   .modal-content h1 { font-size: 18px; }
   .modal-body { display: flex; flex-direction: column; align-items: center; gap: 10px; }
   #wepp-logo { border-radius: 8px; }
@@ -17,27 +17,29 @@ const IOS_MODAL_CONTENT = `
           <img id="wepp-logo" alt="logo" width="64" height="64" />
           <h2 id="wepp-name"></h2>
         </div>
-        <p style="display: flex">
+        <p style="margin-block:4px;">
           1. 브라우저 주소 표시줄에
-          <svg
-            version="1.1"
-            viewBox="0 0 2048 2048"
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              transform="translate(600,655)"
-              d="m0 0h301l1 2v79l-1 1-284 1-17 2-10 5-6 5-6 10-3 11v751l3 12 7 10 8 6 8 3 12 2h776l54-1 12-4 9-7 6-10 3-14v-744l-2-12-5-10-5-6-14-7-4-1-18-1-278-1-1-1v-81h301l18 3 16 5 16 8 12 8 12 10v2l3 1 9 11 9 14 8 17 4 14 2 12 1 15v744l-2 21-6 21-9 19-10 14-12 13-10 8-11 7-14 7-18 6-17 3-13 1h-824l-20-2-19-5-16-7-12-7-16-13-11-12-10-15-8-16-5-17-3-18-1-42v-658l1-61 3-18 5-16 4-9 7-13 8-11 3-4h2l2-4 13-12 16-10 15-7 18-5z"
-            />
-            <path
-              transform="translate(1023,229)"
-              d="m0 0 5 3 269 269 1 4-53 53-2 3-4-1-172-172-1 3v753l-2 3h-81l-1-2v-758l-174 174h-3l-7-8-48-48 1-5 10-9 162-162 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 8-7z"
-            />
-          </svg>
+          <span style="vertical-align:middle;">
+            <svg
+              version="1.1"
+              viewBox="0 0 2048 2048"
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                transform="translate(600,655)"
+                d="m0 0h301l1 2v79l-1 1-284 1-17 2-10 5-6 5-6 10-3 11v751l3 12 7 10 8 6 8 3 12 2h776l54-1 12-4 9-7 6-10 3-14v-744l-2-12-5-10-5-6-14-7-4-1-18-1-278-1-1-1v-81h301l18 3 16 5 16 8 12 8 12 10v2l3 1 9 11 9 14 8 17 4 14 2 12 1 15v744l-2 21-6 21-9 19-10 14-12 13-10 8-11 7-14 7-18 6-17 3-13 1h-824l-20-2-19-5-16-7-12-7-16-13-11-12-10-15-8-16-5-17-3-18-1-42v-658l1-61 3-18 5-16 4-9 7-13 8-11 3-4h2l2-4 13-12 16-10 15-7 18-5z"
+              />
+              <path
+                transform="translate(1023,229)"
+                d="m0 0 5 3 269 269 1 4-53 53-2 3-4-1-172-172-1 3v753l-2 3h-81l-1-2v-758l-174 174h-3l-7-8-48-48 1-5 10-9 162-162 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 8-7z"
+              />
+            </svg>
+          </span>
           아이콘을 클릭하세요.
         </p>
-        <p>2. '홈 화면에 추가'를 눌러주세요.</p>
+        <p style="margin-block:4px;">2. '홈 화면에 추가'를 눌러주세요.</p>
 
         <button id="ok-button">괜찮아요, 모바일 웹으로 볼게요.</button>
       </div>
